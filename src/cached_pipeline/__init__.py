72376b802d22cdd76bc19d4e4bc71fc5efd3db3b
Hinerk <hinerk.stender@mail.de>
2024-04-03 14:29:35 +0200

    working draft

--- src/cached_pipeline/__init__.py
+++ src/cached_pipeline/__init__.py
@@ -1,16 +1,31 @@
+import asyncio
 import functools
 import inspect
 import enum
 from typing import TypeVar
 from typing import Protocol
 from typing import Generic
-from collections.abc import Coroutine
-from collections.abc import Callable
+from typing import Callable
+from logging import getLogger
 
 
-_T = TypeVar('_T')
+logger = getLogger(__name__)
+
+
 _TaskReturnType = TypeVar('_TaskReturnType')
 _CacheIdType = TypeVar('_CacheIdType')
+
+
+class _Function(Protocol):
+    def __call__(self, *args, **kwargs) -> _TaskReturnType: ...
+
+
+class _ReadCacheFunction(Protocol):
+    def __call__(self) -> _TaskReturnType: ...
+
+
+class _WriteCacheFunction(Protocol):
+    def __call__(self,  data: _TaskReturnType) -> None: ...
 
 
 class WhatToReturn(enum.Enum):
@@ -19,30 +34,17 @@ class WhatToReturn(enum.Enum):
     CALCULATED_DATA = enum.auto()
 
 
-class PipelineTask(Generic[_TaskReturnType, _CacheIdType]):
-    class _Function(Protocol):
-        def __call__(self, *args, **kwargs) -> _TaskReturnType: ...
+class PipelineTask(Generic[_TaskReturnType]):
+    def __init__(self):
+        self._func = None
+        self._read_cache = None
+        self._write_cache = None
+        self._cache_id = None
 
-    class _ReadCacheFunction(Protocol):
-        def __call__(self, cache_id: _CacheIdType) -> _TaskReturnType: ...
-
-    class _WriteCacheFunction(Protocol):
-        def __call__(self,
-                     data: _TaskReturnType,
-                     cache_id: _CacheIdType) -> None:
-            ...
-
-    def __init__(self,
-                 func: _Function,
-                 read_cache: _ReadCacheFunction | None = None,
-                 write_cache: _WriteCacheFunction | None = None,
-                 cache_id: str | None = None):
+    def _function(self, func):
         self._func = func
-        self._read_cache = read_cache
-        self._write_cache = write_cache
-        if cache_id is None:
-            cache_id = func.__name__
-        self._cache_id = cache_id
+        functools.update_wrapper(self, self._func)
+        return func
 
     def read_cache(self, func: _ReadCacheFunction) -> _ReadCacheFunction:
         """decorates a custom read cache function"""
@@ -56,11 +58,21 @@ class PipelineTask(Generic[_TaskReturnType, _CacheIdType]):
 
     def __call__(self, *args, **kwargs) -> _TaskReturnType:
         data = self._func(*args, **kwargs)
-        self._write_cache(data=data, cache_id=self._cache_id)
+        self._write_cache(data=data)
         return data
 
     def cached_data(self) -> _TaskReturnType:
-        return self._read_cache(cache_id=self._cache_id)
+        return self._read_cache()
+
+
+class AsyncPipelineTask(PipelineTask):
+    async def __call__(self, *args, **kwargs) -> _TaskReturnType:
+        data = await self._func(*args, **kwargs)
+        await self._write_cache(data=data)
+        return data
+
+    async def cached_data(self) -> _TaskReturnType:
+        return await self._read_cache()
 
 
 class CachedPipeline:
@@ -68,14 +80,12 @@ class CachedPipeline:
         self._return_cached_data = False
         self._read_cache_func = None
         self._write_cache_func = None
-        self._use_cache_until_task = None
         self._skip_until_task = None
+        self._cache_aliases = dict()
 
     def skip_until_task(self, task):
-        if callable(task):
-            self._skip_until_task = task.__name__
-        else:
-            self._skip_until_task = task
+        self._skip_until_task = self._cache_aliases[task]
+        logger.debug("skipping execution until %s", self._skip_until_task)
 
     def enable_caching(self):
         """enable the use of cached data"""
@@ -87,83 +97,89 @@ class CachedPipeline:
         self._skip_until_task = None
 
     def read_cache(self, func):
+        """decorates generic read-from-cache function"""
         self._read_cache_func = func
         return func
 
     def write_cache(self, func):
+        """decorates generic write-to-cache function"""
         self._write_cache_func = func
         return func
 
-    def _sync_read_cache(self, cache_id):
-        if inspect.iscoroutinefunction(self._read_cache_func):
-            raise RuntimeError("Na!")
-        return self._read_cache_func(cache_id=cache_id)
-
-    def _sync_write_cache(self, data, cache_id):
-        if inspect.iscoroutinefunction(self._write_cache_func):
-            raise RuntimeError("Na!")
-        return self._write_cache_func(data=data, cache_id=cache_id)
-
-    async def _async_read_cache(self, cache_id):
-        if inspect.iscoroutinefunction(self._read_cache_func):
-            return await self._read_cache_func(cache_id=cache_id)
-        return self._read_cache_func(cache_id=cache_id)
-
-    async def _async_write_cache(self, data, cache_id):
-        if inspect.iscoroutinefunction(self._write_cache_func):
-            return await self._write_cache_func(data=data, cache_id=cache_id)
-        return self._write_cache_func(data=data, cache_id=cache_id)
-
-    def task(self, _func=None, cache_id=None):
+    def task(self, _func=None, cache_id=None) -> PipelineTask | Callable[[_Function], PipelineTask]:
         """cache response of decorated function"""
         if cache_id is None:
             cache_id = _func.__name__
 
-        def what_to_return_logic():
-            if self._skip_until_task is not None:
-                if self._skip_until_task == cache_id:
-                    self._skip_until_task = None
+        def decorator(func: _Function):
+            if asyncio.iscoroutinefunction(func):
+                task = AsyncPipelineTask()
+            else:
+                task = PipelineTask()
+
+            def what_to_return_logic():
+                if self._skip_until_task is not None:
+                    if self._skip_until_task == cache_id:
+                        self._skip_until_task = None
+                        return WhatToReturn.CACHED_DATA
+                    return WhatToReturn.NOTHING
+                if self._return_cached_data:
                     return WhatToReturn.CACHED_DATA
-                return WhatToReturn.NOTHING
-            if self._return_cached_data:
-                return WhatToReturn.CACHED_DATA
-            return WhatToReturn.CALCULATED_DATA
+                return WhatToReturn.CALCULATED_DATA
 
-        def decorator(func: Callable[[...], _T] | Coroutine[[...], _T]):
-            @functools.wraps(func)
-            def sync_wrapper(*args, **kwargs):
-                what_to_return = what_to_return_logic()
+            if asyncio.iscoroutinefunction(func):
+                @task._function
+                @functools.wraps(func)
+                async def async_wrapper(*args, **kwargs):
+                    what_to_return = what_to_return_logic()
 
-                if what_to_return == WhatToReturn.CACHED_DATA:
-                    return self._sync_read_cache(cache_id=cache_id)
-                elif what_to_return == WhatToReturn.NOTHING:
-                    return None
-                elif what_to_return == WhatToReturn.CALCULATED_DATA:
-                    refined_data = func(*args, **kwargs)
-                    self._sync_write_cache(refined_data, cache_id)
-                    return refined_data
+                    if what_to_return == WhatToReturn.CACHED_DATA:
+                        return await task.cached_data()
+                    elif what_to_return == WhatToReturn.NOTHING:
+                        return None
+                    elif what_to_return == WhatToReturn.CALCULATED_DATA:
+                        return await func(*args, **kwargs)
 
-                raise RuntimeError(f'not clear how to handle {what_to_return}')
+                    raise RuntimeError(
+                        f'not clear how to handle {what_to_return}')
 
-            @functools.wraps(func)
-            async def async_wrapper(*args, **kwargs):
-                what_to_return = what_to_return_logic()
+                @task.read_cache
+                async def read_cache() -> _TaskReturnType:
+                    if inspect.iscoroutinefunction(self._read_cache_func):
+                        return await self._read_cache_func(cache_id=cache_id)
+                    return self._read_cache_func(cache_id=cache_id)
 
-                if what_to_return == WhatToReturn.CACHED_DATA:
-                    return await self._async_read_cache(cache_id=cache_id)
-                elif what_to_return == WhatToReturn.NOTHING:
-                    return None
-                elif what_to_return == WhatToReturn.CALCULATED_DATA:
-                    refined_data = await func(*args, **kwargs)
-                    await self._async_write_cache(refined_data, cache_id)
-                    return refined_data
+                @task.write_cache
+                async def write_cache(data: _TaskReturnType):
+                    if inspect.iscoroutinefunction(self._write_cache_func):
+                        await self._write_cache_func(data=data, cache_id=cache_id)
+                    return await self._write_cache_func(data=data, cache_id=cache_id)
+            else:
+                @task._function
+                @functools.wraps(func)
+                def sync_wrapper(*args, **kwargs):
+                    what_to_return = what_to_return_logic()
 
-                raise RuntimeError(f'not clear how to handle {what_to_return}')
+                    if what_to_return == WhatToReturn.CACHED_DATA:
+                        return task.cached_data()
+                    elif what_to_return == WhatToReturn.NOTHING:
+                        return None
+                    elif what_to_return == WhatToReturn.CALCULATED_DATA:
+                        return func(*args, **kwargs)
 
-            if inspect.iscoroutinefunction(func):
-                return async_wrapper
+                    raise RuntimeError(
+                        f'not clear how to handle {what_to_return}')
 
-            return sync_wrapper
+                @task.read_cache
+                def read_cache() -> _TaskReturnType:
+                    return self._read_cache_func(cache_id=cache_id)
+
+                @task.write_cache
+                def write_cache(data: _TaskReturnType):
+                    self._write_cache_func(data=data, cache_id=cache_id)
+
+            self._cache_aliases[task] = cache_id
+            return task
 
         # allow to decorate function with or without parameter:
         # if _func is callable, then it is assumed, _func was decorated without
