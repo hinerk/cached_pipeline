9dd945374d1db9facd3a689f1658cba592a4ab7c
Hinerk <hinerk.stender@mail.de>
2024-04-03 14:29:33 +0200

    sketched idea

--- src/cached_pipeline/__init__.py
+++ src/cached_pipeline/__init__.py
@@ -1,19 +1,3 @@
-import inspect
-from typing import Callable
-
-
-def _wrap_async_or_sync_function(wrapped_func, sync_wrapper, async_wrapper):
-    if inspect.iscoroutinefunction(wrapped_func):
-        return async_wrapper
-    else:
-        return sync_wrapper
-
-
-def async_if_async(func, *args, **kwargs):
-    if inspect.iscoroutinefunction(func):
-        return
-
-
 class CachedPipeline:
     def __init__(self):
         self._return_cached_data = False
@@ -28,13 +12,15 @@ class CachedPipeline:
         ...
 
     def task(self, func):
+        cache_id = func.__name__
+
         def wrapper(*args, **kwargs):
             if self._return_cached_data:
-                return self.read_cache(cache_id=func)
+                return self.read_cache(cache_id=cache_id)
 
             refined_data = func(*args, **kwargs)
 
-            self.write_cache(refined_data, cache_id=func)
+            self.write_cache(refined_data, cache_id=cache_id)
 
             return refined_data
 
