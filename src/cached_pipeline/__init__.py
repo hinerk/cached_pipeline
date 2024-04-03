aa03f855901b828e67b99f96b2a08c46a8a73815
Hinerk <hinerk.stender@mail.de>
2024-04-03 14:29:34 +0200

    refine procedure to determine whether to return cached or calculated data

--- src/cached_pipeline/__init__.py
+++ src/cached_pipeline/__init__.py
@@ -1,5 +1,6 @@
 import functools
 import inspect
+import enum
 from typing import TypeVar
 from collections.abc import Coroutine
 from collections.abc import Callable
@@ -8,35 +9,34 @@ from collections.abc import Callable
 _T = TypeVar('_T')
 
 
+class WhatToReturn(enum.Enum):
+    NOTHING = enum.auto()
+    CACHED_DATA = enum.auto()
+    CALCULATED_DATA = enum.auto()
+
+
 class CachedPipeline:
     def __init__(self):
         self._return_cached_data = False
         self._read_cache_func = None
         self._write_cache_func = None
         self._use_cache_until_task = None
+        self._skip_until_task = None
 
-    def use_cache(self, use_cache_until_task=None):
-        """
-        enable the usage of the cache for the pipeline
+    def skip_until_task(self, task):
+        if callable(task):
+            self._skip_until_task = task.__name__
+        else:
+            self._skip_until_task = task
 
-        if parameter `use_cache_until_task` is provided, all preceding tasks in
-        the pipeline will use cached values, starting with the given one,
-        all following tasks will run without caching.
-
-        :param use_cache_until_task:
-        :return:
-        """
+    def enable_caching(self):
+        """enable the use of cached data"""
         self._return_cached_data = True
-        if use_cache_until_task is not None:
-            if callable(use_cache_until_task):
-                self._use_cache_until_task = use_cache_until_task.__name__
-            else:
-                self._use_cache_until_task = use_cache_until_task
 
     def disable_caching(self):
         """disable the use of cached data"""
         self._return_cached_data = False
-        self._use_cache_until_task = None
+        self._skip_until_task = None
 
     def read_cache(self, func):
         self._read_cache_func = func
@@ -71,28 +71,46 @@ class CachedPipeline:
         if cache_id is None:
             cache_id = _func.__name__
 
+        def what_to_return_logic():
+            if self._skip_until_task is not None:
+                if self._skip_until_task == cache_id:
+                    self._skip_until_task = None
+                    return WhatToReturn.CACHED_DATA
+                return WhatToReturn.NOTHING
+            if self._return_cached_data:
+                return WhatToReturn.CACHED_DATA
+            return WhatToReturn.CALCULATED_DATA
+
         def decorator(func: Callable[[...], _T] | Coroutine[[...], _T]):
             @functools.wraps(func)
             def sync_wrapper(*args, **kwargs):
-                if self._use_cache_until_task == cache_id:
-                    self._return_cached_data = False
+                what_to_return = what_to_return_logic()
 
-                if self._return_cached_data:
+                if what_to_return == WhatToReturn.CACHED_DATA:
                     return self._sync_read_cache(cache_id=cache_id)
-                refined_data = func(*args, **kwargs)
-                self._sync_write_cache(refined_data, cache_id)
-                return refined_data
+                elif what_to_return == WhatToReturn.NOTHING:
+                    return None
+                elif what_to_return == WhatToReturn.CALCULATED_DATA:
+                    refined_data = func(*args, **kwargs)
+                    self._sync_write_cache(refined_data, cache_id)
+                    return refined_data
+
+                raise RuntimeError(f'not clear how to handle {what_to_return}')
 
             @functools.wraps(func)
             async def async_wrapper(*args, **kwargs):
-                if self._use_cache_until_task == cache_id:
-                    self._return_cached_data = False
+                what_to_return = what_to_return_logic()
 
-                if self._return_cached_data:
+                if what_to_return == WhatToReturn.CACHED_DATA:
                     return await self._async_read_cache(cache_id=cache_id)
-                refined_data = await func(*args, **kwargs)
-                await self._async_write_cache(refined_data, cache_id)
-                return refined_data
+                elif what_to_return == WhatToReturn.NOTHING:
+                    return None
+                elif what_to_return == WhatToReturn.CALCULATED_DATA:
+                    refined_data = await func(*args, **kwargs)
+                    await self._async_write_cache(refined_data, cache_id)
+                    return refined_data
+
+                raise RuntimeError(f'not clear how to handle {what_to_return}')
 
             if inspect.iscoroutinefunction(func):
                 return async_wrapper
